6921521daa705d43e18987585c2ec7b29fcc393d
Viktor Belous <vyntyk@yandex.ru>
2022-08-08 17:52:10 +0300

    Массив  позиция

--- Papka6/Program.cs
+++ Papka6/Program.cs
@@ -9,18 +9,40 @@
         index++;
     }
 }
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 void PrintArray(int[] col)
 {
     int count = col.Length;
     int position = 0;
-    while(position < count)
+    while (position < count)
     {
         Console.WriteLine(col[position]);
-        position ++;
+        position++;
     }
 }
+
 FillArray(array);
 PrintArray(array);
- 
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine("Позиция: " + pos);
