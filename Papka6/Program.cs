2ecd319af9997b1ceb2c872509bf3090bda2ddb3
Viktor Belous <vyntyk@yandex.ru>
2022-08-08 17:39:19 +0300

    Массив и Рандом

---
+++ Papka6/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+int[] array = new int[10];
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position ++;
+    }
+}
+FillArray(array);
+PrintArray(array);
+ 
